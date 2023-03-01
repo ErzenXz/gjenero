54e7ab0c7b1a77d85b0a8bbc5f5f5e93d2df6971
ErzenXs <njnana2017@gmail.com>
2023-03-02 00:27:49 +0100

    Bug fixes.

--- image/script.js
+++ image/script.js
@@ -1,5 +1,9 @@
 const modelUrl =
    "https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_100_224/classification/4";
+
+// const modelUrl = "https://tfhub.dev/google/imagenet/inception_v3/classification/5";
+// const modelUrl = "https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/classification/5";
+
 let model;
 
 let min = 0;
@@ -12,6 +16,7 @@ if (prog.value != undefined || prog.value != null) {
 
 async function loadModel() {
    model = await mobilenet.load();
+   document.getElementById("mini").classList.remove("hidden");
    console.log("Model loaded");
 }
 
@@ -23,11 +28,14 @@ async function classifyImages(images) {
       const predictions = await model.classify(img);
       prog.value += 1;
       const imageTags = predictions
-         .map((p) => p.className.replace(/,/g, ""))
+         .map((p) => p.className.replace(/,/g, "").split(" "))
+         .flat()
          .filter((c) => c.indexOf("'") === -1)
-         .slice(0, 5); // keep top 5 tags
+         .slice(0, 5);
 
-      tags.push({ tags: imageTags, orientation: img.height > img.width ? "V" : "H" });
+      const uniqueTags = [...new Set(imageTags)];
+
+      tags.push({ tags: uniqueTags, orientation: img.height > img.width ? "V" : "H" });
    }
 
    return tags;
@@ -54,12 +62,16 @@ function downloadTxtFile(tags) {
       date.getMinutes() +
       ":" +
       date.getSeconds();
+
+   document.getElementById("output").innerHTML = content;
+
    element.href = URL.createObjectURL(file);
    element.download = `tags-${dateTime}.txt`;
    document.body.appendChild(element);
-   element.click();
+   //element.click();
    document.getElementById("fileInput").classList.remove("hidden");
    document.getElementById("running").classList.add("hidden");
+   document.getElementById("results").classList.remove("hidden");
 }
 
 async function handleFileUpload(event) {
@@ -72,6 +84,7 @@ async function handleFileUpload(event) {
 
    document.getElementById("fileInput").classList.add("hidden");
    document.getElementById("running").classList.remove("hidden");
+   document.getElementById("results").classList.add("hidden");
 
    for (let i = 0; i < files.length; i++) {
       const file = files[i];
@@ -91,6 +104,25 @@ async function handleFileUpload(event) {
 
       reader.readAsDataURL(file);
    }
+}
+
+function saveFile() {
+   const element = document.createElement("a");
+   const file = new Blob([document.getElementById("output").value], { type: "text/plain" });
+   let date = new Date();
+   let dateTime =
+      date.getTime() +
+      date.getUTCSeconds() +
+      "/ " +
+      date.getHours() +
+      date.getMinutes() +
+      ":" +
+      date.getSeconds();
+
+   element.href = URL.createObjectURL(file);
+   element.download = `tags-${dateTime}.txt`;
+   document.body.appendChild(element);
+   element.click();
 }
 
 document.getElementById("fileInput").addEventListener("change", handleFileUpload);
